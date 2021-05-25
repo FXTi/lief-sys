de4e2bfffbfc4cf0c82e1d86a8fc790c1474367f
FXTi <zjxiang1998@gmail.com>
2021-05-25 23:35:37 +0800

    Remove PartialEq

--- build.rs
+++ build.rs
@@ -39,18 +39,8 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header(lief_c_header.to_string_lossy())
         .clang_arg(format!("-I{}", dst.join("include").display()))
-        //.prepend_enum_name(false)
-        //.rustified_enum("*")
-        .derive_partialord(true)
-        .no_partialeq(
-            "max_align_t|__fsid_t|imaxdiv_t|\
-            __crt_locale_data_public|__crt_locale_pointers|__crt_locale_pointers|_Mbstatet|_Lldiv_t|__crt_locale_data|__crt_multibyte_data|\
-            Macho_Header_t|Macho_Command_t|Macho_Symbol_t|Macho_Section_t|Macho_Segment_t|Macho_Binary_t|\
-            Pe_DosHeader_t|Pe_Header_t|Pe_OptionalHeader_t|Pe_DataDirectory_t|\
-            Pe_Section_t|Pe_ImportEntry_t|Pe_Import_t|Pe_Binary_t|\
-            Elf_Section_t|Elf_Segment_t|Elf_Header_t|Elf_DynamicEntry_t|Elf_DynamicEntry_Library_t|\
-            Elf_DynamicEntry_SharedObject_t|Elf_DynamicEntry_Array_t|Elf_DynamicEntry_Rpath_t|\
-            Elf_DynamicEntry_RunPath_t|Elf_DynamicEntry_Flags_t|Elf_Symbol_t|Elf_Binary_t")
+        .prepend_enum_name(false)
+        .rustified_enum("*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
